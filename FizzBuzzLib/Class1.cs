fa506eb7fdbf9d981c3e3080655c6958234952d1
exacon <tteigenes@gmail.com>
2013-05-15 23:36:36 +0200

    first commit?

---
+++ FizzBuzzLib/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzLib
+{
+    public class FizzBuzzClass
+    {
+
+
+        public bool IsFizz(int p)
+        {
+            return(p % 3 == 0 ? true: false);
+        }
+
+        public bool IsBuzz(int p)
+        {
+            return (p % 5 == 0 ? true : false);
+        }
+
+        public bool isFizzBuzz(int p)
+        {
+            return (IsFizz(p) && IsBuzz(p) ? true : false);
+                        
+        }
+
+        public string runFizzBuzz()
+        {
+            string result = "";
+
+            for (int i = 1; i < 100; i++)
+            {
+                string temp = "";
+
+                if (isFizzBuzz(i))
+                {
+                    temp = "FizzBuzz";
+                }
+                else if (IsFizz(i))
+                {
+                    temp = "Fizz";
+                }
+                else if (IsBuzz(i))
+                {
+                    temp = "Buzz";
+                }
+                else
+                    temp = i.ToString();
+
+                result = result + " " + temp;
+
+            }
+            //Test commit to github
+            return result;
+        }
+    }
+}
---
+++ ZeBigTestClass/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FizzBuzzLib;
+
+
+namespace ZeBigTestClass
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            FizzBuzzClass testclass = new FizzBuzzClass();
+            Assert.IsNotNull(testclass);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            FizzBuzzClass test = new FizzBuzzClass();
+            Assert.IsTrue(test.IsFizz(3));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            FizzBuzzClass test = new FizzBuzzClass();
+            Assert.IsTrue(test.IsBuzz(5));
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            FizzBuzzClass test = new FizzBuzzClass();
+            Assert.IsTrue(test.isFizzBuzz(15));
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            FizzBuzzClass test = new FizzBuzzClass();
+            Assert.IsInstanceOfType(test.runFizzBuzz(),typeof(string));
+
+        }
+
+    }
+}
